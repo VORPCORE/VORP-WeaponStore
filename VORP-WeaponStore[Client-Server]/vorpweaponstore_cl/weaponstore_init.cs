266b75c9ceab73ec5e3b33d6e330fe0c3f7e701a
Osmany González <devbiitz2@gmail.com>
2020-06-03 19:43:12 +0100

    Pre Config and Init

---
+++ VORP-WeaponStore[Client-Server]/vorpweaponstore_cl/weaponstore_init.cs
@@ -0,0 +1,17 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vorpweaponstore_cl
+{
+    class weaponstore_init : BaseScript
+    {
+        public weaponstore_init()
+        {
+
+        }
+    }
+}
---
+++ VORP-WeaponStore[Client-Server]/vorpweaponstore_sv/weaponstore_init_sv.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vorpweaponstore_sv
+{
+    public class weaponstore_init_sv 
+    {
+    }
+}
