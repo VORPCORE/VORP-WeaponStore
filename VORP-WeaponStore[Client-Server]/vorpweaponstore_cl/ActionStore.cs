f122e91125f11ce660ed30d36fcb959d4f2569b6
Osmany González <devbiitz2@gmail.com>
2020-06-07 02:45:39 +0100

    Added Static Vars

--- VORP-WeaponStore[Client-Server]/vorpweaponstore_cl/ActionStore.cs
+++ VORP-WeaponStore[Client-Server]/vorpweaponstore_cl/ActionStore.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CitizenFX.Core.Native.API;
+using MenuAPI;
 
 namespace vorpweaponstore_cl
 {
     public class ActionStore : BaseScript
     {
+        private static int ObjectStore;
+        private static int CamStore;
+        private static int LaststoreId;
 
     }
 }
