1b31755b2ee62cccb5dc4f80ba08866691747f85
Osmany González <devbiitz2@gmail.com>
2020-06-07 03:19:02 +0100

    Clear Cache onStopRes

---
+++ VORP-WeaponStore[Client-Server]/vorpweaponstore_cl/ClearCaches.cs
@@ -0,0 +1,38 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CitizenFX.Core.Native.API;
+
+namespace vorpweaponstore_cl
+{
+    class ClearCaches : BaseScript
+    {
+        public ClearCaches()
+        {
+            EventHandlers["onResourceStop"] += new Action<string>(OnResourceStop);
+        }
+
+        private void OnResourceStop(string resourceName)
+        {
+            if (GetCurrentResourceName() != resourceName) return;
+
+            Debug.WriteLine($"{resourceName} cleared blips and NPC's.");
+
+            foreach (int blip in weaponstore_init.StoreBlips)
+            {
+                int _blip = blip;
+                RemoveBlip(ref _blip);
+            }
+
+            foreach (int npc in weaponstore_init.StorePeds)
+            {
+                int _ped = npc;
+                DeletePed(ref _ped);
+            }
+        }
+
+    }
+}
