4984499ccdd09c2ed3488e3da310168b8e9f518e
Osmany González <devbiitz2@gmail.com>
2020-06-26 18:29:46 +0100

    Create LoadInYourDB.sql

---
+++ VORP-WeaponStore[Client-Server]/build/vorp_weaponstore/LoadInYourDB.sql
@@ -0,0 +1,10 @@
+INSERT INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES
+('ammo_bullet_pistol', 'Munición de Pistola', 10, 1, 'item_standard', 1),
+('ammo_bullet_revolver', 'Munición de Revolver', 10, 1, 'item_standard', 1),
+('ammo_bullet_rifle', 'Munición de Rifle', 1, 1, 'item_standard', 1),
+('ammo_bullet_varmint', 'Munición de Varmint', 1, 1, 'item_standard', 1),
+('ammo_shotgun', 'Munición de Escopeta', 10, 1, 'item_standard', 1),
+('banana', 'Platano', 6, 1, 'item_standard', 1),
+('bread', 'Pan', 10, 1, 'item_standard', 1),
+('consumable_haycube', 'Heno', 50, 1, 'item_standard', 1),
+('water', 'Agua', 500, 1, 'item_standard', 1);
